3ba38cb0152c61a7cee192a6ab86c683f33c7a9a
shah nawaas <shanuclashofc@gmail.com>
2024-07-18 18:51:23 +0530

    Added Assignment 10

---
+++ index.js
@@ -0,0 +1,52 @@
+const userAge = 20;
+const userName = "John";
+
+console.log(userAge,userName);
+
+/*
+
+java script is a programming language used to create a dynamic language
+
+*/
+
+let num1 = 10;
+let num2 = 30;
+
+console.log("Addition:", num1 + num2);
+console.log("Subraction:",num2 - num1);
+console.log("Multiplication:",num1*num2);
+console.log("Division;",num2/num1);
+
+let arr = [1,2,true];
+let str = "John";
+let bool = true;
+let num = 5;
+
+console.log(arr);
+console.log(str);
+console.log(bool);
+console.log(num);
+
+function greetUser(name){
+    let a = "Welcomes you all"
+    return a 
+};
+
+console.log(greetUser());
+
+let temp = 34;
+if (temp>30){
+    console.log("Temparature is high");
+
+}else{
+    console.log("Temparature is normal");
+};
+
+for (let i=1; i <= 5; i++) {
+    console.log(i)
+};
+
+/*
+Loose equality operator is used to assign a value
+But strict eqaulity is used to check the data type
+*/
